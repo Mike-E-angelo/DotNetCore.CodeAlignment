5355af5b2c94c648107b90382daaf3369e6a2d16
Mike-EEE <michael@dragonspark.us>
2018-05-13 16:12:45 -0400

    Removing publish file. Does not belong here, dohp.

--- Publish.cmd
+++
@@ -1,3 +0,0 @@
-dotnet publish DotNetCore.CodeAlignment -c Release
-robocopy DotNetCore.CodeAlignment\bin\Release\netcoreapp2.1\publish P:\DotNetCore.CodeAlignment\ /S /PURGE /MT
-robocopy DotNetCore.CodeAlignment\bin\Release\netcoreapp2.1\publish Q:\DotNetCore.CodeAlignment\ /S /PURGE /MT
